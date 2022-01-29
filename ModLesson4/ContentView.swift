3c6e87e186474a1f2fe5734be841a1242d8febd3
Anand.Narayan <arnarayan@gmail.com>
2022-01-29 09:50:06 -0700

    completed the challenge for lesson 4 in module 2

--- ModLesson4/ContentView.swift
+++ ModLesson4/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var addedNumbers:[Int] = []
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        
+
+        VStack {
+
+        NavigationView {
+            List(addedNumbers, id: \.self) {elem in
+                Text(String(elem))
+                }.navigationTitle("Growing Pains")
+        }
+        HStack {
+            Button(action: {
+                var checkOut = Int.random(in: 1...10)
+                while checkOut != 7 {
+                addedNumbers.append(checkOut)
+                checkOut = Int.random(in: 1...10)
+            }
+            },
+                label: {
+                    Text("Add")
+            })
+            Button(action: {
+                for (index,element) in addedNumbers.enumerated() {
+                addedNumbers[index] = element+1
+            }
+            },
+                label: {
+                    Text("Increase")
+            })
+            Button(action: {
+                addedNumbers.removeAll()
+            },
+                label: {
+                    Text("Clear")
+            })
+
+        }
+
+
+
+        }
+
+        
+        
+        
     }
 }
 
